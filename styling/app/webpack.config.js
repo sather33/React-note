22d6f6277d98d5bb74e233ed80ab234a5d9c860e
sather33 <court.dream3@gmail.com>
2019-01-07 18:13:31 +0800

    add dev-server

--- styling/app/webpack.config.js
+++ styling/app/webpack.config.js
@@ -13,6 +13,9 @@ module.exports = {
       exclude: /node_modules/
     }]
   },
-  devtool: 'cheap-module-eval-source-map'
+  devtool: 'cheap-module-eval-source-map',
   //cheap-module-eval-source-map is a easy way to debug console error.
+  devServer: {
+    contentBase:  path.join(__dirname, 'public'),
+  },
 };
